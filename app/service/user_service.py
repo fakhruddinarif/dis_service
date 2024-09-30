47306c1e7e866df646d9f271404a51f94ab940aa
fakhruddinarif <muhfakhruddinarif30@gmail.com>
2024-09-30 20:28:42 +0700

    modify: check logical register

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -12,20 +12,33 @@ class UserService:
         self.user_repository = UserRepository(db)
 
     def register(self, request: RegisterUserRequest) -> UserSchema:
+        errors = {}
+        required_fields = {
+            "name": "Name is required",
+            "email": "Email is required",
+            "password": "Password is required",
+            "phone": "Phone is required"
+        }
+
+        for field, error_message in required_fields.items():
+            if not getattr(request, field):
+                errors[field] = error_message
+
+        if self.user_repository.find_by_email(request.email):
+            errors["email"] = "Email already exists"
+        if self.user_repository.find_by_phone(request.phone):
+            errors["phone"] = "Phone already exists"
+
+        if errors:
+            raise HTTPException(status_code=400, detail=errors)
+
         try:
-            if not request.name or not request.email or not request.password:
-                raise HTTPException(status_code=400, detail="Invalid request")
-            if self.user_repository.find_by_email(request.email):
-                raise HTTPException(status_code=400, detail="Email already registered")
             password = pwd_context.hash(request.password)
             user = User(name=request.name, email=request.email, password=password, phone=request.phone)
             self.user_repository.create(user)
             self.user_repository.db.commit()
             self.user_repository.db.refresh(user)
             return UserSchema.from_orm(user)
-        except HTTPException as err:
-            self.user_repository.db.rollback()
-            raise err
         except Exception as err:
             self.user_repository.db.rollback()
             raise HTTPException(status_code=500, detail=str(err))
