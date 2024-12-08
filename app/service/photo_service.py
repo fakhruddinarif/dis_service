d1dcdf3a0a13096161d12d34c6a1cede72739058
fakhruddinarif <muhfakhruddinarif30@gmail.com>
2024-12-09 06:04:27 +0700

    setup deployment

--- app/service/photo_service.py
+++ app/service/photo_service.py
@@ -146,14 +146,12 @@ class PhotoService:
         logger.info(f"List photo request: {request}")
         try:
             photos, total = self.photo_repository.list(request)
-            logger.info(f"List photo response: {photos}")
             if request.type == "sell":
                 for photo in photos:
                     photo["url"] = s3_client.get_object(config.aws_bucket, urlparse(photo["url"]).path.lstrip("/"))
                     photo["_id"] = str(photo["_id"])
                     photo["user_id"] = str(photo["user_id"])
                     photo["buyer_id"] = str(photo["buyer_id"]) if photo["buyer_id"] else None
-                logger.info(f"List sell photo response: {photos}")
                 return [SellPhotoResponse(**photo).dict(by_alias=True) for photo in photos], total
             else:
                 for photo in photos:
@@ -200,7 +198,6 @@ class PhotoService:
                 raise HTTPException(status_code=400, detail="Error during update photo")
             photo.id = str(photo.id)
             photo.user_id = str(photo.user_id)
-            logger.info(f"Update post photo response: {photo}")
             return PostPhotoResponse(**photo.dict(by_alias=True))
         except Exception as e:
             logger.error(f"Error during update post photo: {str(e)}")
--- seeder/main.py
+++ seeder/main.py
@@ -8,7 +8,7 @@ from seeder.user_seeder import seed_users, seed_accounts
 from seeder.photo_seeder import seed_sell_photos, seed_post_photos
 from seeder.transaction_seeder import seed_transactions
 # Seed users and accounts
-# seed_users()
-# seed_accounts()
+seed_users()
+seed_accounts()
 
-seed_transactions()
+# seed_transactions()
