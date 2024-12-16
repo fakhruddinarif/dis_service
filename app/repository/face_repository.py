33e5971e7052dc30db8e3489bd86600dc54b6b78
fakhruddinarif <muhfakhruddinarif30@gmail.com>
2024-12-17 05:26:59 +0700

    feat: list face

--- app/repository/face_repository.py
+++ app/repository/face_repository.py
@@ -1,5 +1,6 @@
 from bson import ObjectId
 
+from app.core.logger import logger
 from app.core.database import database
 from app.repository.base_repository import BaseRepository
 from app.schema.face_schema import ListFaceRequest
@@ -10,7 +11,7 @@ class FaceRepository(BaseRepository):
         super().__init__(database.get_collection("faces"))
 
     def list(self, request: ListFaceRequest):
-        query = {"user_id": request.user_id}
+        query = {"user_id": ObjectId(request.user_id)}
         page = request.page if request.page else 1
         size = request.size if request.size else 10
         skip = (page - 1) * size
@@ -28,7 +29,8 @@ class FaceRepository(BaseRepository):
 
         total = list(self.collection.aggregate(total_pipeline))
         total = total[0]["total"] if total else 0
-        return faces_cursor, total
+        faces = [face for face in faces_cursor]
+        return faces, total
 
     def find_by_user_id(self, user_id: ObjectId):
         return self.collection.find_one({"user_id": user_id})
